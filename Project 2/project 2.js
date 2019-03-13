594cdc9f7dff4066917a81def89c27ed1f787735
scytheofvyse <31118364+scytheofvyse@users.noreply.github.com>
2019-03-13 21:17:37 +0700

    Add files via upload

---
+++ Project 2/project 2.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    document.getElementById("dear").innerHTML = "Sakmioff";
+}
+
+function myHeight(){
+    document.getElementById("tinggiBadan").innerHTML = "";
+
+    var tinggiBadan = 150;
+        if (tinggiBadan > 150){
+            console.log("Silahkan Masuk");
+        }
+        else {
+            console.log("Maaf Tinggi Badan Anda Tidak Mencukupi")
+
+        }
+}
+
+var asda = Math.abs
